36ad39da2c92545ac0ed34992bf4ceb9c96aa7a4
ALLann123 <karisallan237@gmail.com>
2025-08-14 10:50:59 +0300

    ts

---
+++ Type Script/dynamically_typed.js
@@ -0,0 +1,9 @@
+// Dynamically typed- Means type ofvariables can changeat runtime
+
+let myVariable = 10; //the initial variable is a number
+
+console.log(`The value of myVariable is: ${myVariable} and its type is: ${typeof myVariable}`)
+
+//Change t he type of varibale to a string
+myVariable = "Allan"; //now it is a string
+console.log(`The value of myVariable is: ${myVariable} and its type is: ${typeof myVariable}`)
